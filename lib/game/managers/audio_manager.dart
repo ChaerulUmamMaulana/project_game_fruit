e09d379a332428cd6efface3f014b07c8cdbdcb8
chaerulumammaulana <1123150132@global.ac.id>
2026-02-13 07:37:37 -0800

    create toggleMusic

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -95,4 +95,13 @@ void setSfxVolume(double volume) {
 _sfxVolume = volume.clamp(0.0, 1.0);
 }
 
+void toggleMusic() {
+_isMusicEnabled = !_isMusicEnabled;
+if (_isMusicEnabled) {
+resumeBackroundMusic();
+} else {
+pauseBackgroundMusic();
+}
+}
+
 }
