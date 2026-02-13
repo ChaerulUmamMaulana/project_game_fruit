9d0135f587fcbaa5606e79584286bba3a5a52a29
chaerulumammaulana <1123150132@global.ac.id>
2026-02-13 07:34:42 -0800

    create void setMusicVolume

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -82,4 +82,13 @@ class AudioManager {
     }
   }
 
+  void setMusicVolume(double volume) {
+_musicVolume = volume.clamp(0.0, 1.0);
+try {
+FlameAudio.bgm.audioPlayer.setVolume(_musicVolume);
+} catch (e) {
+print('Error setting music volume: $e');
+}
+}
+
 }
