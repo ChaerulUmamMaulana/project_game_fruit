fdb9bdb40698398597a6d5e1d5084e915e2fbd04
chaerulumammaulana <1123150132@global.ac.id>
2026-02-13 06:25:48 -0800

    create getters pada file audio manager

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -7,4 +7,9 @@ class AudioManager {
   bool _isSfxEnabled = true;
   double _musicVolume = 0.7;
   double _sfxVolume = 1.0;
+
+  bool get isMusicEnabled => _isMusicEnabled;
+  bool get isSfxEnabled => _isSfxEnabled;
+  double get musicVolume => _musicVolume;
+  double get sfxVolume => _sfxVolume;
 }
