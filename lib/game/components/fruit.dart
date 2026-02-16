28627da9e389706ce7cebf6f62f3cf0e1bd467f2
chaerulumammaulana <1123150132@global.ac.id>
2026-02-15 22:47:43 -0800

    update shine paint pada file fruit.dart

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -66,5 +66,9 @@ class Fruit extends PositionComponent with HasGameRef<FruitCatcherGame>, Collisi
         }
 
         canvas.drawCircle(Offset(size.x / 2, size.y / 2), size.x / 2, paint);
+
+        final shinePaint = Paint()
+        ..color = Colors.white.withOpacity(0,3)
+        ..style = PaintingStyle.fill;
       }
 }
