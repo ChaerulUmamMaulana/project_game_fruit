f786eff7f5c2d23010af87fde74c92ebcc0f5d6c
chaerulumammaulana <1123150132@global.ac.id>
2026-02-15 23:05:32 -0800

    update pada class fruitcatchergame yaitu late basket sampai double fruitSpawanInterval

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,13 +1,22 @@
+import 'dart:math';
 import 'dart:ui';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
+import 'package:project_game_fruit/game/components/basket.dart';
 import 'package:project_game_fruit/game/managers/audio_manager.dart';
 
-class FruitCatcherGame extends FlameGame {
-  @override
-  Color backgroundColor() => const Color(0xFF87CEEB);
+class FruitCatcherGame extends FlameGame with PanDetector, HasCollisionDetection {
+    late Basket basket;
+    late TextComponent scoreText;
+    final Random random = Random();
+    double fruitSpawnInterval = 1.5;
+
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  
 
   @override
   Future<void> onLoad() async {
